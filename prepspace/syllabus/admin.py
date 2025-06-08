a82d198d234402067d8434e646d69fdcb048746e
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2025-06-08 12:33:40 +0530

    registered subject and topic model on the admin site

--- prepspace/syllabus/admin.py
+++ prepspace/syllabus/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Subject, Topic
 
-# Register your models here.
+# Register your models here so that they can be shown on Django Admin Dashboard
+admin.site.register(Subject)
+admin.site.register(Topic)
