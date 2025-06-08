749e906aeb3882709fa0b05a1411582c4ec41762
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2025-06-08 13:12:07 +0530

    included urls of syllabus app

--- prepspace/prepspace/urls.py
+++ prepspace/prepspace/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .views import dashboard
+from syllabus import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path ("", dashboard),
+    path("syllabus", include(urls)), # include urls of syllabus app and the urlpattern in syllabus/{}
 ]
