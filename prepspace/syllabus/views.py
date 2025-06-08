c788f6c7a6aeea741672a90ed80dfa929ff34a22
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2025-06-08 13:15:16 +0530

    created syllabus_home view to render syllabus_home.html

--- prepspace/syllabus/views.py
+++ prepspace/syllabus/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Subject
 
 # Create your views here.
+def syllabus_home(request):
+    subjects = Subject.objects.all()
+    return render(request,'syllabus_home.html',{'subjects': subjects} )
