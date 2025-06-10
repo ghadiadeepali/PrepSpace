af9777338e2e033986d35f2630861678a8526aaa
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2025-06-10 12:44:35 +0530

    set upload_to as subject_images/ to save user uploaded files

--- prepspace/syllabus/models.py
+++ prepspace/syllabus/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Subject(models.Model):
     subject_name = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='', null=True, blank=True)
+    image = models.ImageField(upload_to='subject_images/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
