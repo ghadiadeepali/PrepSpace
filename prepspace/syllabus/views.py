d7700e429611b31ac4300344d0ef544078bbefda
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2025-06-10 14:15:54 +0530

    created view for detailed_syllabus

--- prepspace/syllabus/views.py
+++ prepspace/syllabus/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Subject
+from .models import Subject, Topic
 
 # Create your views here.
 def syllabus_home(request):
     subjects = Subject.objects.all()
     return render(request,'syllabus_home.html',{'subjects': subjects} )
+
+def view_subject_details(request, subject_id):
+    topics = Topic.objects.filter(subject_id=subject_id)
+    return render(request, 'subject_details.html', {'topics': topics} )
